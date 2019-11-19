1304673d99ecc6bb1de1d452aad0dc023e8a2834
Kory Prince <korylprince@gmail.com>
2019-11-19 16:23:00 -0600

    properly encapsulate setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export PRINTERMANAGER_SQLDSN="host=$POSTGRES_HOST port=$POSTGRES_PORT user=$POSTGRES_USER password=$POSTGRES_PASS dbname=$POSTGRES_DATABASE sslmode=disable"
+export PRINTERMANAGER_SQLDSN="host='$POSTGRES_HOST' port='$POSTGRES_PORT' user='$POSTGRES_USER' password='$POSTGRES_PASS' dbname='$POSTGRES_DATABASE' sslmode=disable"
 
 exec $1
