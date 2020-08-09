fff108ad110f08ce3aa48bd2ae6b9165d6f811fb
Kory Prince <korylprince@gmail.com>
2020-08-08 22:31:39 -0500

    fixed some schema issues

--- schema/printer.sql
+++ schema/printer.sql
@@ -1,12 +1,12 @@
 CREATE TABLE manufacturer (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR UNIQUE NOT NULL CHECK (1 < char_length(name) AND char_length(name) < 256)
+    name VARCHAR UNIQUE NOT NULL CHECK (1 <= char_length(name) AND char_length(name) < 256)
 );
 
 CREATE TABLE model (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     manufacturer_id UUID NOT NULL,
-    name VARCHAR NOT NULL CHECK (1 < char_length(name) AND char_length(name) < 256),
+    name VARCHAR NOT NULL CHECK (1 <= char_length(name) AND char_length(name) < 256),
     driver JSONB NOT NULL,
     UNIQUE(manufacturer_id, name),
     FOREIGN KEY (manufacturer_id) REFERENCES manufacturer(id)
@@ -16,7 +16,7 @@ CREATE TABLE printer (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     model_id UUID NOT NULL,
     location_id UUID NOT NULL,
-    hostname VARCHAR NOT NULL CHECK (1 < char_length(hostname) AND char_length(hostname) < 256),
+    hostname VARCHAR NOT NULL CHECK (1 <= char_length(hostname) AND char_length(hostname) < 256),
     driver_extra JSONB,
     UNIQUE(model_id, location_id, hostname),
     FOREIGN KEY (model_id) REFERENCES model(id),
@@ -39,3 +39,36 @@ CREATE VIEW printer_flattened AS
         printer.location_id = location.id
     INNER JOIN building ON
         location.building_id = building.id;
+
+CREATE VIEW user_printers AS
+    SELECT
+        assigned.user_id,
+        printer_flattened.*
+    FROM printer_flattened INNER JOIN ((
+        SELECT
+            "user".id AS user_id,
+            printer.id AS printer_id
+        FROM "user"
+        INNER JOIN location_user_assignment ON
+            "user".id = location_user_assignment.user_id
+        INNER JOIN location ON
+            location_user_assignment.location_id = location.id
+        INNER JOIN printer ON
+            location.id = printer.location_id
+    ) UNION (
+        SELECT
+            "user".id AS user_id,
+            printer.id AS printer_id
+        FROM "user"
+        INNER JOIN group_user_assignment ON
+            "user".id = group_user_assignment.user_id
+        INNER JOIN "group" ON
+            group_user_assignment."group_id" = "group".id
+        INNER JOIN location_group_assignment ON
+            "group".id = location_group_assignment."group_id"
+        INNER JOIN location ON
+            location_group_assignment.location_id = location.id
+        INNER JOIN printer ON
+            location.id = printer.location_id
+    )) AS assigned ON
+        printer_flattened.id = assigned.printer_id;
--- schema/user.sql
+++ schema/user.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "user" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     local_id VARCHAR(255) UNIQUE NOT NULL,
-    display_name VARCHAR NOT NULL CHECK (1 < char_length(display_name) AND char_length(display_name) < 256),
-    username VARCHAR NOT NULL CHECK (1 < char_length(username) AND char_length(username) < 256)
+    display_name VARCHAR NOT NULL CHECK (1 <= char_length(display_name) AND char_length(display_name) < 256),
+    username VARCHAR NOT NULL CHECK (1 <= char_length(username) AND char_length(username) < 256)
 );
 
 CREATE TABLE location_user_assignment (
@@ -16,7 +16,7 @@ CREATE TABLE location_user_assignment (
 CREATE TABLE "group" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     local_id VARCHAR(255) UNIQUE NOT NULL,
-    display_name VARCHAR NOT NULL CHECK (1 < char_length(display_name) AND char_length(display_name) < 256)
+    display_name VARCHAR NOT NULL CHECK (1 <= char_length(display_name) AND char_length(display_name) < 256)
 );
 
 CREATE TABLE group_user_assignment (
@@ -34,36 +34,3 @@ CREATE TABLE location_group_assignment (
     FOREIGN KEY (location_id) REFERENCES location(id),
     FOREIGN KEY ("group_id") REFERENCES "group"(id) ON DELETE CASCADE
 );
-
-CREATE VIEW user_printers AS
-    SELECT
-        assigned.user_id,
-        printer_flattened.*
-    FROM printer_flattened INNER JOIN ((
-        SELECT
-            "user".id AS user_id,
-            printer.id AS printer_id
-        FROM "user"
-        INNER JOIN location_user_assignment ON
-            "user".id = location_user_assignment.user_id
-        INNER JOIN location ON
-            location_user_assignment.location_id = location.id
-        INNER JOIN printer ON
-            location.id = printer.location_id
-    ) UNION (
-        SELECT
-            "user".id AS user_id,
-            printer.id AS printer_id
-        FROM "user"
-        INNER JOIN group_user_assignment ON
-            "user".id = group_user_assignment.user_id
-        INNER JOIN "group" ON
-            group_user_assignment."group_id" = "group".id
-        INNER JOIN location_group_assignment ON
-            "group".id = location_group_assignment."group_id"
-        INNER JOIN location ON
-            location_group_assignment.location_id = location.id
-        INNER JOIN printer ON
-            location.id = printer.location_id
-    )) AS assigned ON
-        printer_flattened.id = assigned.printer_id;
